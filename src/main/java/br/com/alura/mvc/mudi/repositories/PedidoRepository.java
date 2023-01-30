9dbc418d7e47872075fb9e8681bed7295aeb8d7c
William Ramos <contato.williamramos@outlook.com>
2023-01-30 02:44:24 -0300

    Ajustando PedidoRepository para utilizar Spring Data

--- src/main/java/br/com/alura/mvc/mudi/repositories/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repositories/PedidoRepository.java
@@ -3,25 +3,19 @@ package br.com.alura.mvc.mudi.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.model.User;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
-	@Query("select p from Pedido p "
-			+ "join p.user u "
-			+ "where u.username = :username "
-				+ "and p.status = :status")
-	List<Pedido> findByStatus(
-			@Param("status") StatusPedido status,
-			@Param("username") String username);
+	List<Pedido> findByStatus(StatusPedido status);
 
-	@Query("select p from Pedido p join p.user u where u.username = :username")
-	List<Pedido> findAllByUsuario(@Param("username") String username);
+	List<Pedido> findByUser(User user);
+
+	List<Pedido> findByStatusAndUser(StatusPedido status, User user);
 
 }
